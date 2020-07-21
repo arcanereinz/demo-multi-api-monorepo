40a1aa8ce73a4800e0bb2b30f39791494c2840cf
Nang Chan <nchan@kixie.com>
2020-07-21 13:15:33 -0700

    Update user/accounts

--- accounts/src/getAccount/index.js
+++ accounts/src/getAccount/index.js
@@ -1,6 +1,11 @@
 exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+    // Log the event argument for debugging and for use in local development.
+    console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+    return {
+        statusCode: 200,
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ source: 'accounts' }),
+        isBase64Encoded: false,
+    };
 };
